e23bf70a550779749268ef082444583ee4c8c299
yuxiaoyang <532028476@qq.com>
2020-08-09 17:16:33 +0800

    ttttt

---
+++ src/main/java/com/fh/shop/api/student/IStudentMapper.java
@@ -0,0 +1,4 @@
+package com.fh.shop.api.student;
+
+public interface IStudentMapper {
+}
--- src/main/java/com/fh/shop/api/student/Student.java
+++ src/main/java/com/fh/shop/api/student/Student.java
@@ -4,6 +4,7 @@ public class Student {
 
     public static void main(String[] args) {
         System.out.println("======================");
+        System.out.println("*********************");
     }
 
 }
