fab7d59b56fa06a3e464dff4f0bce32cb1f39f70
yuxiaoyang <532028476@qq.com>
2020-08-16 22:43:34 +0800

    update

--- src/main/java/com/fh/shop/api/ShopApiApplication.java
+++ src/main/java/com/fh/shop/api/ShopApiApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @MapperScan("com.fh.shop.api.*.mapper")
 //@EnableScheduling
 public class ShopApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ShopApiApplication.class, args);
     }
